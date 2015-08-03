ff596aaf665e27f6d98255e6447730a3a0b0b1f3
Will Fortin <fortinw4@gmail.com>
2015-08-02 22:31:23 -0700

    'push w/o handlebars'

--- index.js
+++ index.js
@@ -2,16 +2,118 @@
 
 $(function(){
 
-	var usersPath = 'http://localhost:3000/users/'
-	var uid = $('').attr('data-id')
+	var usersPath = 'http://localhost:3000/users'
+	var permPath = 'http://localhost:3000/permissions'
+	var homePath = 'http://localhost:8080/'
 
-	$.get(usersPath).done(function(users) {
-		console.log(users)
+	$('.users-btn').on('click', function() {
+		$('.permissions').hide()
+		$.get(usersPath).done(function(users) {
+			users.forEach(function(user) {
+				// console.log(user)
+				$('.users').append('<li data-id="' + user.id + '" data-name="' + user.name + '"> ' + user.name + ' <a href="#">Permissions</a></li>' + '<br>')
+			})	
+		})		
 	})
 
-	$('button').on('click', function() {
-		$('body').append('<li>Brad - <a href="#">Get Permissions</a></li>')
+	$('ul').on('click', 'a', function() {
+		var uname = $(this).parents('li').data('name')
+		var uid = $(this).parents('li').data('id')
+		$('.page1').text('')
+		$('.page2').show()
+		$('h1').append(uname)
+		$.get(permPath).done(function(perms) {
+			perms.forEach(function(perm) {
+				if (uid === perm.userId) {
+					$('.page2').append('<li>' + perm.permissions + '</li>')
+				}
+			})
+		})
+		$('.home-btn').on('click', function () {
+			window.location = homePath
+		})
 	})
+})	
 
-})
 
+
+		// $.get(usersPath).done(function(users) {
+		// 	users.forEach(function(user) {
+		// 		console.log(user)
+		// 	})
+		// })
+
+
+// $.get(permPath).done(function(perms) {
+		// 	perms.forEach(function(perm) {
+		// 		$('ul').append(perm.permissions + '<br>')
+		// 	})
+		// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// $.get(usersPath).done(function(users) {
+	// 	console.log(users)
+	// 	users.forEach(function(user) {
+	// 		// $.get(usersPath + users.id + '/posts')
+	// 		// 	.done(function (userPosts) {
+	// 		// 		console.log(user.name)
+	// 		// 	})
+	// 	})
+	// })
+
+
+// })		
+			// $('body').append('<li>Brad - <a href="#">Get Permissions</a></li>')
+			// $('body').append('<li>Brig - <a href="#">Get Permissions</a></li>')
+			// $('body').append('<li>Dave - <a href="#">Get Permissions</a></li>')
+			// $('body').append('<li>Sam - <a href="#">Get Permissions</a></li>')
+// 		})
+
+// })
+
+
+// getUsers
+// 	getPerm
+// 		loop over users
+// 			loop over permissions
+// 				compare does currentPerm match currentUserId
+
+// 				OR
+
+// getUsers
+// 	loop over users
+// 		getPermForUser(id)
+// 			currentUser.perms = currentPermissions
+
+// $.get(//users)
+// 	.done(function (users) {
+// 		$.get(//permissions)
+// 			.done(function(perms) {
+// 				users.forEach(function (currentUser) {
+// 					currentUser.perms = []
+// 					perms.forEach(function (currentPerm)) {
+// 						console.log('does it match?', currentUser.id === currentPerm.userId)
+
+// 						if (currentUser.id === currentPerm.userId) {
+// 							currentUser.perms.push(currentPerm)
+// 						}
+// 					}
+// 				})
+// 			})
+// 		 console.log('done looping')
+// 		 console.log('users', users)
+// 	})
+
+// var tmpl = Handlebars.compile ({{name}} has {{#perms}}: {{create}}) {{read}} {{update}}
